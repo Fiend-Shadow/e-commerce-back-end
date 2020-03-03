5684dd2ac2a07c505b5a0bb9ddf79ec2be4790f9
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-03-03 17:20:32 +0100

    Add Post and Get on project-routes.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,44 @@
 const express = require("express");
-const ProductRouter = express.Router();
+const productRouter = express.Router();
+
 const createError = require("http-errors");
 const Product = require("../models/productsModel");
 
+const mongoose = require ('mongoose');
+
+
+//POST 
+productRouter.post('/searchPage', (req, res, next) => {
+  const {productName, category, priceRange} = req.body;
+
+  Product.create( { productName, category, priceRange } )
+    .then( (createdProduct) => {
+			res
+				.status(201) // Created
+				.json(createdProduct);			
+		})
+		.catch( (err) => {
+			res
+				.status(500)
+				.json(err);
+		});
+});
+
+
+//GET
+productRouter.get('/searchPage/:category', (req, res, next) = {
+	
+	Product.find({category: req.params})
+	  .then( (projectsByCategory) => {
+			res
+				.status(200)
+				.json(projectsByCategory)
+		})
+		.catch( (err) => {
+			res.status(500).json(err);
+		});
+})
+
+
+
+module.exports = productRouter;
