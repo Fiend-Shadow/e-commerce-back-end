cd82da42cae6f4c49c12b8f55dedb0c01b895496
fiend-shadow <tarek.koushaji@gmail.com>
2020-03-04 11:44:37 +0100

    create orderRouter
    create orderRoutes: all orders, cancel and order , updated quantity for one product in a specific order , and creat an order

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const orderRouter = express.Router();
 const createError = require("http-errors");
+
 const Order = require("../models/ordersModel");
 const User = require("./../models/userModel");
 
@@ -40,6 +41,70 @@ orderRouter.post("/create" , (req,res,next) => {
     
 });
 
+orderRouter.get("/allOrders" , (req,res,next) => {
+    const {_id} = req.session.currentUser ;
+    User.findById({_id}).populate("orders")
+    .then((oneUser) => {
+        res
+        .status(200)
+        .json(oneUser.orders);
+    }).catch((err) => {
+        res
+        .status(500)
+        .json(createError(err));
+    });
+});
+
+orderRouter.delete("/cancelOrder" , (req,res,next) => {
+    const {_id} = req.body;
+    const userId = req.session.currentUser._id;
+    Order.findById({_id})
+    .then((oneOrder) => {
+        if (oneOrder.orderUser === userId && oneOrder.isDelivered === false)
+        {
+            Order.findByIdAndRemove({_id})
+            .then(() => {
+                res
+                .status(204)
+                .send();
+            })
+        }
+    }).catch((err) => {
+        res
+        .status(500)
+        .json(createError(err));
+    });
+});
+
+orderRouter.put("/updateQuantity" , (req,res,next) => {
+    const {_id , productId , plus , minus} = req.body;
+    Order.findById({_id})
+    .then((oneOrder) => {
+        let updatedArray = oneOrder.orderProducts;
+        updatedArray.map((oneProduct, index)=>{
+            if (oneProduct.id == productId){
+                let updatedProduct = updatedArray.slice(index,index+1);
+                if (plus == true){
+                    updatedProduct.quantity ++ ;
+                }
+                else if(minus == true){
+                    updatedProduct.quantity --;
+                }
+                updatedArray = updatedArray.unshift(updatedProduct)
+                Order.findByIdAndUpdate({_id} , {$set : {"orderProducts" : updatedArray}  } , {new : true})
+                .then((updatedOrder)=>{
+                    res
+                    .status(200)
+                    .json(updatedOrder);
+                })
+            }
+        })
+    }).catch((err) => {
+        res
+        .status(500)
+        .json(createError(err));
+    });
+});
 
 
 module.exports = orderRouter;
